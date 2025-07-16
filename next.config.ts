565c50030eb4d0b85608873651919cb97bcaadd7
Dilshan-Somaweera <147154867+Dilshan-Somaweera@users.noreply.github.com>
2025-07-16 19:52:26 +0530

    Update next.config.ts
    modified repo name with setting base path in order to show  the images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const repoName = "portfolio_with_githubactions"
+  
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", 
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
